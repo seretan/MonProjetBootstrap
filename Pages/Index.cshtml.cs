b43293871454c339d4d4b3c129310984c7d27202
Violeta Seretan <seretan@users.noreply.github.com>
2026-08-17 17:15:46 +0200

    Add back-end generated 'Hello World' message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class IndexModel : PageModel
 {
     public string MessageBienvenue { get; set; } = string.Empty;
+    public string ModalMessage { get; set; } = string.Empty;
 
     public void OnGet()
     {
         // Logique métier ou appel de service en C#
         MessageBienvenue = "Bonjour, propulsé par C# .NET !";
+        ModalMessage = "Hello World";
     }
 }
